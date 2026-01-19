20986ff4a7eb2170ef5d78c878a1bc9ed423324d
Aiden Shankar <aiden.vc2015@gmail.com>
2026-01-19 16:23:33 +0000

    Manual code change

--- src/pages/LearnerDashboard.jsx
+++ src/pages/LearnerDashboard.jsx
@@ -4,7 +4,7 @@ export default function LearnerDashboard() {
   return (
     <div className="w-full h-[calc(100vh-64px)] bg-black">
       <iframe
-        src="https://schoolace-frontend-963696778204.us-central1.run.app/$0"
+        src="https://schoolace-frontend-963696778204.us-central1.run.app/"
         className="w-full h-full border-none"
         title="Learner Dashboard"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; camera; microphone"
