ea47ed4ab4d5c8e8d8f8fb41318e80ff447c7c58
Aiden Shankar <aiden.vc2015@gmail.com>
2026-01-30 09:19:00 +0000

    New Themes Indicator Moved

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1262,7 +1262,6 @@ Output your response as JSON with:
                             {/* Show supercharged badge for students (non-interactive) */}
                             {user.app_role === 'student' && (
                                 <>
-                                    <div className="relative">
                                     <Button 
                                         onClick={() => {}} 
                                         className="text-white text-xs px-3 py-3 rounded-full shadow-lg font-bold tracking-wider cursor-default h-10"
@@ -1271,13 +1270,7 @@ Output your response as JSON with:
                                     >
                                         SUPERCHARGED
                                     </Button>
-                                    <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 flex flex-col items-center pointer-events-none animate-bounce z-50">
-                                        <div className="w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[8px] border-b-white"></div>
-                                        <div className="bg-white text-indigo-600 text-xs font-bold px-2 py-1 rounded shadow-lg whitespace-nowrap">
-                                            New Themes!
-                                        </div>
-                                    </div>
-                                    </div>
+                                    <div className="relative">
                                     <Dialog>
                                         <DialogTrigger asChild>
                                             <Button 
@@ -1327,6 +1320,13 @@ Output your response as JSON with:
                                             </div>
                                         </DialogContent>
                                     </Dialog>
+                                    <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 flex flex-col items-center pointer-events-none animate-bounce z-50">
+                                        <div className="w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[8px] border-b-white"></div>
+                                        <div className="bg-white text-indigo-600 text-xs font-bold px-2 py-1 rounded shadow-lg whitespace-nowrap">
+                                            New Themes!
+                                        </div>
+                                    </div>
+                                    </div>
                                     <Button 
                                         variant="ghost"
                                         size="icon"
@@ -1356,7 +1356,6 @@ Output your response as JSON with:
                                 </>
                                 )}
                             {user.app_role === 'teacher' && (
-                                <div className="relative">
                                 <Button 
                                     onClick={() => {}} 
                                     className="text-white text-xs px-3 py-3 rounded-full shadow-lg font-bold tracking-wider cursor-default h-10"
@@ -1365,15 +1364,9 @@ Output your response as JSON with:
                                 >
                                     SUPERCHARGED
                                 </Button>
-                                <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 flex flex-col items-center pointer-events-none animate-bounce z-50">
-                                    <div className="w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[8px] border-b-white"></div>
-                                    <div className="bg-white text-indigo-600 text-xs font-bold px-2 py-1 rounded shadow-lg whitespace-nowrap">
-                                        New Themes!
-                                    </div>
-                                </div>
-                                </div>
                             )}
                             {user.app_role === 'teacher' && (
+                                <div className="relative">
                                 <Dialog>
                                     <DialogTrigger asChild>
                                         <Button 
@@ -1449,6 +1442,13 @@ Output your response as JSON with:
                                             </div>
                                                 </DialogContent>
                                 </Dialog>
+                                <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 flex flex-col items-center pointer-events-none animate-bounce z-50">
+                                    <div className="w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[8px] border-b-white"></div>
+                                    <div className="bg-white text-indigo-600 text-xs font-bold px-2 py-1 rounded shadow-lg whitespace-nowrap">
+                                        New Themes!
+                                    </div>
+                                </div>
+                                </div>
                             )}
                             </div>
 
