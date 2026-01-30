5d04734925fe8e2ef8bd9b3019b34afdac58badb
Aiden Shankar <aiden.vc2015@gmail.com>
2026-01-30 05:44:27 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -69,7 +69,6 @@ import Setup from './pages/Setup';
 import TermsOfService from './pages/TermsOfService';
 import compliance from './pages/compliance';
 import examplelearningtracker from './pages/examplelearningtracker';
-import Dashboard from './pages/Dashboard';
 import __Layout from './Layout.jsx';
 
 
@@ -96,7 +95,6 @@ export const PAGES = {
     "TermsOfService": TermsOfService,
     "compliance": compliance,
     "examplelearningtracker": examplelearningtracker,
-    "Dashboard": Dashboard,
 }
 
 export const pagesConfig = {
