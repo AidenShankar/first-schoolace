d16ab931f9c6eb6130d7ec10c19bc2bba0d2b076
Aiden Shankar <aiden.vc2015@gmail.com>
2026-01-30 16:15:35 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1290,12 +1290,7 @@ Output your response as JSON with:
                                                 <Settings className="w-4 h-4" />
                                             </Button>
                                         </DialogTrigger>
-                                        <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 flex flex-col items-center pointer-events-none animate-bounce z-50">
-                                            <div className="w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[8px] border-b-white"></div>
-                                            <div className="bg-white text-indigo-600 text-xs font-bold px-2 py-1 rounded shadow-lg whitespace-nowrap">
-                                                New Themes!
-                                            </div>
-                                        </div>
+
                                         </div>
                                         <DialogContent>
                                             <DialogHeader>
@@ -1324,10 +1319,15 @@ Output your response as JSON with:
                                                     <ThemeSelector />
                                                 </div>
                                             </div>
-                                        </DialogContent>
-                                        </Dialog>
-                                        </div>
-                                        <Button 
+                                            </DialogContent>
+                                            </Dialog>
+                                            <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 flex flex-col items-center pointer-events-none animate-bounce z-50">
+                                             <div className="w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[8px] border-b-white"></div>
+                                             <div className="bg-white text-indigo-600 text-xs font-bold px-2 py-1 rounded shadow-lg whitespace-nowrap">
+                                                 New Themes!
+                                             </div>
+                                            </div>
+                                            <Button 
                                         variant="ghost"
                                         size="icon"
                                         onClick={() => {
@@ -1387,13 +1387,6 @@ Output your response as JSON with:
                                             <Settings className="w-4 h-4" />
                                         </Button>
                                         </DialogTrigger>
-                                        <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 flex flex-col items-center pointer-events-none animate-bounce z-50">
-                                        <div className="w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[8px] border-b-white"></div>
-                                        <div className="bg-white text-indigo-600 text-xs font-bold px-2 py-1 rounded shadow-lg whitespace-nowrap">
-                                            New Themes!
-                                        </div>
-                                        </div>
-                                        </div>
                                         <DialogContent>
                                         <DialogHeader>
                                             <DialogTitle>Class Settings</DialogTitle>
