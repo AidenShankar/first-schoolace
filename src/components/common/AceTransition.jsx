1f74988c49b7dd2c10d376e0ae48355f312f75fb
Aiden Shankar <aiden.vc2015@gmail.com>
2026-01-31 03:38:47 +0000

    Manual code change

--- src/components/common/AceTransition.jsx
+++ src/components/common/AceTransition.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { BrainCircuit } from 'lucide-react';
 
 const FACTS = [
-    '"You got this!"'
+    '"Yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present"'
 ];
 
 export default function AceTransition() {
