0625044a53a33cbca430038dca65bedfb2e1fc0e
Aiden Shankar <aiden.vc2015@gmail.com>
2026-02-03 02:39:38 +0000

    Update facts in AceTransition

--- src/pages.config.js
+++ src/pages.config.js
@@ -56,6 +56,7 @@ import Chat from './pages/Chat';
 import ChooseCourses from './pages/ChooseCourses';
 import ClassTools from './pages/ClassTools';
 import Compliance from './pages/Compliance';
+import Dashboard from './pages/Dashboard';
 import Demo from './pages/Demo';
 import Gradebook from './pages/Gradebook';
 import Landing from './pages/Landing';
@@ -71,7 +72,6 @@ import StudySet from './pages/StudySet';
 import TermsOfService from './pages/TermsOfService';
 import compliance from './pages/compliance';
 import examplelearningtracker from './pages/examplelearningtracker';
-import Dashboard from './pages/Dashboard';
 import __Layout from './Layout.jsx';
 
 
@@ -85,6 +85,7 @@ export const PAGES = {
     "ChooseCourses": ChooseCourses,
     "ClassTools": ClassTools,
     "Compliance": Compliance,
+    "Dashboard": Dashboard,
     "Demo": Demo,
     "Gradebook": Gradebook,
     "Landing": Landing,
@@ -100,7 +101,6 @@ export const PAGES = {
     "TermsOfService": TermsOfService,
     "compliance": compliance,
     "examplelearningtracker": examplelearningtracker,
-    "Dashboard": Dashboard,
 }
 
 export const pagesConfig = {
