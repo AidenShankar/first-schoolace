aadaf94b08875ec3d4127c3cecd6c1996c90d9b1
Aiden Shankar <aiden.vc2015@gmail.com>
2026-02-21 00:37:03 +0000

    Manual code change

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -5,7 +5,7 @@ import { Home } from 'lucide-react';
 
 export default function Demo() {
   const [loading, setLoading] = useState(true);
-  const demoUrl = "https://mimir-internal-admin-7k6mnc7qga-uc.a.run.app/student-dashboard";
+  const demoUrl = "schoolace.org";
 
   return (
     <div className="fixed inset-0 w-full h-full bg-slate-950 z-50 flex flex-col">
