cec5792545cb7a9d90b5fa7c9b8c89601cd57c6e
Aiden Shankar <aiden.vc2015@gmail.com>
2026-03-10 05:04:41 +0000

    Manual code change

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 export default function Preview() {
   const [loading, setLoading] = useState(true);
-  const previewUrl = "https://mimir-internal-admin-7k6mnc7qga-uc.a.run.app/student-dashboard";
-  //const previewUrl = "https://mimir-internal-admin-7k6mnc7qga-uc.a.run.app/preview";
+  const previewUrl = "http://mimir-internal-admin-7k6mnc7qga-uc.a.run.app/";
+  //const previewUrl = "http://mimir-internal-admin-7k6mnc7qga-uc.a.run.app/";
 
   return (
     <div className="fixed inset-0 w-full h-full bg-slate-950 z-50 flex flex-col">
